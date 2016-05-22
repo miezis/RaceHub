e559b1e85c5265e5ef7f8c7c05aa8dfa4536fb11
miezis <miezinas.mantas@gmail.com>
2016-05-22 17:20:13 +0300

    Transitioning to other components.

--- src/AppBundle/Resources/public/js/app/views/create-race/CreateRaceActions.js
+++ src/AppBundle/Resources/public/js/app/views/create-race/CreateRaceActions.js
@@ -1,10 +1,14 @@
 import app from '../../app';
 import http from '../../utils/http';
+import { browserHistory } from 'react-router'
 
 class CreateRaceActions {
 	create(payload) {
 		return http.post('/races', payload)
 			.then((response) => {
+				if (response.statusCode === 200) {
+					browserHistory.push('/')
+				}
 				console.log(response);
 			})
 	}
