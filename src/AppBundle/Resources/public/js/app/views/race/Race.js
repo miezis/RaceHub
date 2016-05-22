9da8d6deeb3dacdd44a423568a070b1f1c3f8d09
miezis <miezinas.mantas@gmail.com>
2016-05-22 16:09:02 +0300

    Rendering races without polling.

--- src/AppBundle/Resources/public/js/app/views/race/Race.js
+++ src/AppBundle/Resources/public/js/app/views/race/Race.js
@@ -27,9 +27,50 @@ class Race extends react.Component {
 	}
 
 	render() {
+		const race = this.props.race;
+
+		if (!race) {
+			return null;
+		}
+
 		return (
 			<div className="row">
+				<div className="col-md-12">
+					<h2>Event: {race.event_name}</h2>
+				</div>
+				<div className="col-md-12">
+					<p>Class: {race.race_class}</p>
+				</div>
+				<div className="col-md-12">
+					<p>Update every 2 seconds</p>
+				</div>
+				<div className="table-responsive col-md-12">
+					<table className="table table-striped">
+						<thead>
+							<tr>
+								<th>Position</th>
+								<th>Racers Name</th>
+								<th>Total Laps</th>
+								<th>Best Time</th>
+							</tr>
+						</thead>
+						<tbody>
+							{_.map(race.racers, (racer, index) => (this.renderRacerRow(racer, index)))}
+						</tbody>
+					</table>
+				</div>
 			</div>
+		);
+	}
+
+	renderRacerRow(racer, index) {
+		return (
+			<tr>
+				<td>{index + 1}</td>
+				<td>{racer.name}</td>
+				<td>{racer.lapCount}</td>
+				<td>{racer.bestTime / 1000}</td>
+			</tr>
 		);
 	}
 }
